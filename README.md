557eda669552043b0ab18bb7bb4270a735fe4c74
Andrew Zaia <andrew.zaia@outlook.com.au>
2024-03-11 01:27:12 +1100

    Update repo

--- README.md
+++ README.md
@@ -1,152 +1,28 @@
 # AssyriaBot
 
-AssyriaBot is a discord bot that can fetch Bible verses based on user input. It provides a simple interface to look up Bible verses by book, chapter, and verse.
+AssyriaBot is an exclusive bot for the [r/Assyria](https://discord.gg/r-assyria-247923889697587200) Discord community. It provides various features aimed at enhancing the community's experience and promoting Assyrian culture and heritage.
 
-It also provides information about Assyria and Assyrians. This was created for Assyrian based discord communities to bring recognition to the Assyrian people, their culture and their rich ancient history.
+## AlapBet Quiz
 
-This git includes two variants, the Python version or a Ruby version - they are not the same.
+AssyriaBot implements an interactive AlapBet quiz game using Discord's API. The AlapBet quiz aims to educate users about Assyrian characters and their descriptions. Users can initiate the quiz in their own channel, where they will encounter multiple-choice questions related to various characters from Assyrian history and culture.
 
-* The **Python** version uses the `https://bible-api.com/` website and API calls to get the information.
-* The **Ruby** version uses an offline XML file called `eng-web.usfx.xml` (included). This version is currently limited to 2 different variations of calls (more information below).
+This is being expanded into various levels of increasing difficulty.
 
-This git also includes Docker image for ease of running.
+## Information Hub
+
+In addition to the AlapBet quiz, AssyriaBot serves as an information hub about Assyria and Assyrians. It provides resources and insights into Assyrian culture, history, and heritage. This feature was created to foster recognition of the Assyrian people and their rich ancient history within the [r/Assyria](https://discord.gg/r-assyria-247923889697587200) Discord community.
 
 ## Setup
 
-1. Clone the repository.
+At this present time it is not being developed for public use, rather being used as a repository for shared experience with likeminded individuals who wish to contribute to this project for the [r/Assyria](https://discord.gg/r-assyria-247923889697587200) Discord community.
 
-2. Set up a Discord bot and obtain its token. You can find instructions on how to do this here: [Creating a Bot Account](https://discord.com/developers).
+## Data Privacy
 
-3. Install dependencies:
-   * For Python, you can use pip:
-     ```
-     pip install -r requirements.txt
-     ```
-   * For Ruby, you can use bundler:
-     ```
-     bundle install
-     ```
-
-4. Create a `.env` file in the root directory and add your bot token:
-
-```
-API_KEY=
-BOT_TOKEN=
-CHANNEL=
-CLIENT_ID=
-```
-
-## Usage
-
-### Python
-
-* Run the Python script:
-
-```
-python main.py
-```
-
-* The bot will listen for commands prefixed with `!`. For example:
-
-  * Single Verses: `!bible john 3:16`
-  * Entire Chapter: `!bible john 3`
-  * Abbreviated book name: `!bible jn 3:16`
-  * Verse range: `!bible romans 12:1-2`
-  * Multiple ranges: `!bible romans 12:1-2,5-7,9,13:1-9&10`
-  * Random verse: `!bible random`
-
-* The bot will respond with the requested Bible verse.
-
-### Ruby
-
-* Run the Ruby script:
-
-```
-ruby main.rb
-```
-
-* The bot will listen for commands prefixed with `!`. For example:
-
-  * Single Verses: `!bible John 3:16`
-  * Entire Chapter: `!bible John 3`
-
-**Note: Capitalisation in the Ruby variant is important.**
-
-* The bot will respond with the requested Bible verse.
-
-### Docker
-
-Both the Python and Ruby code have Dockerfiles included in the repository. You can use Docker to run the bot in a containerized environment.
-
-#### Python Dockerfile
-
-```
-# Use an official Python runtime as a parent image
-FROM python:3.8
-
-# Set the working directory inside the container
-WORKDIR /app
-
-# Copy the requirements file into the container
-COPY requirements.txt .
-
-# Install any needed packages specified in requirements.txt
-RUN pip install -r requirements.txt
-
-# Copy the rest of your application code into the container
-COPY . .
-
-# Define the command to run your bot
-CMD ["python", "main.py"]
-```
-
-To build the Docker image and run the container:
-
-```
-docker build -t AssyriaBotPython -f Dockerfile\(Python\) .
-docker run -it AssyriaBotPython
-```
-
-#### Ruby Dockerfile
-
-```
-# Use the official Ruby image from the Docker Hub
-FROM ruby:3.1
-
-# Set the working directory in the container
-WORKDIR /usr/src/app
-
-# Install required gems
-COPY Gemfile Gemfile.lock ./
-RUN bundle install
-
-# Copy the Ruby script and XML file into the container
-COPY . .
-
-# Run the Ruby script
-CMD ["ruby", "main.rb"]
-```
-
-To build the Docker image and run the container:
-
-```
-docker build -t AssyriaBotRuby -f Dockerfile\(Python\) .
-docker run -it AssyriaBotRuby
-```
-
-## Assyria
-
-AssyriaBot also includes the `!assyria` command that provides some information about Assyrians and the Assyrian flag in a neat embed.
-
-![!assyria command screenshot](images/assyria.png)
-
-More Assyrian based commands to come!
+AssyriaBot relies on external data sources to build lessons and quizzes within the Discord server. To maintain the integrity and exclusivity of the data, it is not publicly available. This approach ensures that users within the [r/Assyria](https://discord.gg/r-assyria-247923889697587200) Discord community have access to unique and curated content tailored to their interests.
 
 ## Contributing
 
-Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
-
-Please make sure to update tests as appropriate.
+Contributions to AssyriaBot are welcome! If you have ideas for new features, improvements, or fixes, feel free to fork this repository and submit pull requests. Your contributions help enhance the functionality and experience of the bot for the entire community.
 
 ## License
 
--- main.py
+++ main.py
@@ -1,99 +1,225 @@
-import discord
-from discord.ext import commands
+import asyncio
+import mysql.connector
 import os
-import requests
+import random
+
+import nextcord
+from nextcord.ext import commands
 from dotenv import load_dotenv
 
 # Load the .env file
 load_dotenv()
 
-# Access the bot token and channel ID from the environment variables
+# Access the bot and database from the environment variables
 TOKEN = os.getenv('BOT_TOKEN')
 CHANNEL_ID = int(os.getenv('CHANNEL'))
+DB_HOST = os.getenv('host')
+DB_USER = os.getenv('user')
+DB_PASS = os.getenv('password')
+DB_NAME = os.getenv('database')
+
+# MySQL connection
+db = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASS,
+    database=DB_NAME
+)
 
 # Define the intents with the desired flags
-intents = discord.Intents.all()
-intents.messages = True # Activate message intents
+intents = nextcord.Intents.all()
+intents.messages = True  # Activate message intents
+intents.presences = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
 
+# Define a global variable to store the room object
+game_room = None
 
-async def message_send_verses(ctx, reference, message_verses):
-    """
-    A function to send Bible verses as messages.
-    
-    :param ctx: The context in which the message is sent.
-    :param reference: The reference of the Bible verse.
-    :param message_verses: The verses to be sent as messages.
-    """
-    await ctx.send(f'**{reference}**')
-    for verse in message_verses:
-        await ctx.send(f'```{verse}```')
-
+# Read characters and their descriptions from the alapbet.txt file
+async def read_characters_from_file(filename):
+    characters = []
+    with open(filename, 'r', encoding='utf-8') as file:
+        for line in file:
+            character_parts = line.strip().split(';')
+            if len(character_parts) == 5:
+                characters.append(tuple(character_parts))
+    return characters
 
 @bot.event
 async def on_ready():
-    """
-    A function that prints the bot's name when it's logged in.
-    """
     print(f'Logged in as {bot.user.name}')
+    activity = nextcord.Game(f'Assyrian teacher on {len(bot.guilds)} server(s)')
+    await bot.change_presence(status=nextcord.Status.online, activity=activity)
 
-
-@bot.command()
-async def bible(ctx, book: str, chapter: str = None, verse: int = None):
-    """
-    A command that retrieves Bible verses based on user input
-    
-    :param ctx: The context in which the message is sent.
-    :param book: The name of the book in the Bible.
-    :param chapter: The chapter number in the book (optional).
-    :param verse: The verse number in the chapter (optional).
-    """
-    if book.lower() == 'random':
-        url = f"https://bible-api.com/?random=verse"
-    elif verse is None:
-        url = f"https://bible-api.com/{book}{chapter}"
+# Function to update user stats and send an update message
+async def update_user_stats(user_id, total_attempts, total_score, ctx):
+    guild = ctx.guild
+    category = nextcord.utils.get(guild.categories, name='lessons')
+    room = nextcord.utils.get(guild.channels, name=ctx.user.name, category=category)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM user_stats WHERE user_id = %s", (user_id,))
+    result = cursor.fetchone()
+    if result:
+        total_attempts += int(result[4])
+        total_score += int(result[5])
+        cursor.execute("UPDATE user_stats SET total_attempts = %s, total_score = %s WHERE user_id = %s", (total_attempts, total_score, user_id))
     else:
-        url = f"https://bible-api.com/{book}{chapter}:{verse}"
-
-    # url += f"?translation=kjv" # Translation is hard set to King James Version
+        cursor.execute("INSERT INTO user_stats (user_id, username, discriminator, total_attempts, total_score) VALUES (%s, %s, %s, %s, %s)", (user_id, ctx.user.name, ctx.user.discriminator, total_attempts, total_score))
+    db.commit()
+    cursor.close()
     
-    response = requests.get(url)
-    print(url)
-    data = response.json()
+    # Calculate percentage
+    percentage = round((total_score / total_attempts) * 100, 2) if total_attempts > 0 else 0
+    
+    # Send the user an update on their stats
+    stats_message = await game_room.send(f'**{ctx.user.name}**, here\'s an update on your stats:\n'
+                                         f'Number of correct answers: {total_score}\n'
+                                         f'Number of total attempts: {total_attempts}\n'
+                                         f'Percentile: {percentage}%\n\n'
+                                         f'Do you want to keep going?')
 
-    # Delete user's request
-    await ctx.message.delete()
+    # Add reactions for user to continue or stop
+    await stats_message.add_reaction('✅')  # Tick
+    await stats_message.add_reaction('❌')  # Cross
 
-    max_length = 1700
+    # Wait for the user's reaction
+    def check(reaction, user):
+        return user == ctx.user and str(reaction.emoji) in ['✅', '❌'] and reaction.message.id == stats_message.id
 
-    # User validation to ensure data input entry is correct
-    if 'error' in data:
-        await ctx.send("Invalid input. Please use the format `!bible Book Chapter:Verse`, e.g., `!bible John 3:16`.")
-    else:
-        reference = data['reference']
-        # await ctx.send(f'**{reference}**')
-        
-        message_verses = [] # Sets the verses in a list
-        current_verse = '' # Clears the current verse to None
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=10.0, check=check)
 
-        for verse_data in data['verses']:
-            # book_name = verse_data['book_name']
-            chapter = verse_data['chapter']
-            verse = verse_data['verse']
-            text = verse_data['text'].replace('\n', '')
-
-            new_verse = f'[{verse}] {text} '.replace('“', '"').replace('”', '"').replace('‘', '\'').replace('’', '\'')
-            if len(current_verse) + len(new_verse) > max_length:
-                message_verses.append(current_verse)
-                current_verse = new_verse
-            else:
-                current_verse += new_verse
-            
-        if current_verse:
-            message_verses.append(current_verse)
-
-        await message_send_verses(ctx, reference, message_verses)
+        if str(reaction.emoji) == '✅':
+            # Start the game
+            await start_game(ctx)
+        elif str(reaction.emoji) == '❌':
+            await room.send("Stopped! Use `/quiz` to continue.")
+    except asyncio.TimeoutError:
+        await room.send("Stopped! Use `/quiz` to continue.")
 
 
+@bot.slash_command(description="Start the AlapBet Quiz in your own channel!")
+# Function to create a new room under the 'lessons' category with the user's username as the room name
+async def quiz(ctx: nextcord.Interaction):
+    global game_room
+
+    await ctx.send('Starting up now! Scroll to the bottom of channels (on the left - under "lessons") to find your room.', ephemeral=True)
+
+    guild = ctx.guild
+    category = nextcord.utils.get(guild.categories, name='lessons')
+    if not category:
+        # Create the category if it doesn't exist
+        category = await guild.create_category('lessons')
+
+    # Check if the room already exists
+    room = nextcord.utils.get(guild.channels, name=ctx.user.name, category=category)
+    if not room:
+        # Create the room if it doesn't exist
+        room = await category.create_text_channel(name=ctx.user.name)
+
+        # Set permissions for the user who created the room
+        await room.set_permissions(ctx.user, read_messages=True, send_messages=True)
+
+        await room.set_permissions(bot.user, read_messages=True, send_messages=True)
+
+        # Revoke permissions for @everyone
+        await room.set_permissions(guild.default_role, read_messages=False, send_messages=False)
+
+        await room.send(f"Welcome to your lesson room, {ctx.user.name}!")
+
+        print(f'Created room for: {ctx.user.name}')
+
+    # Store the room object in the global variable
+    game_room = room
+
+    # Start the game in the room by passing the original message object
+    await start_game(ctx)
+
+
+# Function to start the game
+async def start_game(ctx):
+    global game_room
+
+    # Load characters from file
+    characters = await read_characters_from_file('alapbet.txt')
+
+    # Pick a random character and get its description
+    character = random.choice(characters)
+    character_name = character[0]
+    character_sound = character[1]
+    character_description = character[3]
+    character_image = character[4]
+
+    # Remove the chosen character from the list
+    characters.remove(character)
+
+    # Pick two other random characters as choices
+    other_characters = random.sample(characters, 2)
+    characters.append(character)  # Add the chosen character back to the list
+
+    # Add the choices to the embed
+    choices = [character_name] + [other[0] for other in other_characters]
+    random.shuffle(choices)
+
+    # Create an embed with the multiple-choice options
+    assyrianFlag = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ef/Flag_of_the_Assyrians.svg/1280px-Flag_of_the_Assyrians.svg.png'
+    embed = nextcord.Embed(
+        description=character_description,
+        color=0x0099FF
+    )
+    embed.set_author(name='Alapbet: Level 1', url=character_image, icon_url=assyrianFlag)
+    embed.set_thumbnail(url=assyrianFlag)
+    embed.add_field(name='**Hint:**', value=f'||{character[0]}||', inline=False)
+    embed.add_field(name='\u200B', value='\u200B', inline=False)
+    embed.add_field(name=f'{choices[0]}', value='1️⃣', inline=True)
+    embed.add_field(name=f'{choices[1]}', value='2️⃣', inline=True)
+    embed.add_field(name=f'{choices[2]}', value='3️⃣', inline=True)
+    embed.set_image(url=character_image)
+    embed.set_footer(text='Please choose the correct answer below', icon_url=assyrianFlag)
+    embed.timestamp = nextcord.utils.utcnow()
+
+    # Send the embed to the channel    
+    message_to_edit = await game_room.send(embed=embed)
+
+    # Add reaction emojis for each option
+    await message_to_edit.add_reaction('1️⃣')
+    await message_to_edit.add_reaction('2️⃣')
+    await message_to_edit.add_reaction('3️⃣')
+
+    # Wait for the user's reaction
+    def check(reaction, user):
+        return user == ctx.user and str(reaction.emoji) in ['1️⃣', '2️⃣', '3️⃣'] and reaction.message.id == message_to_edit.id
+
+    try:
+        reaction, user = await bot.wait_for('reaction_add', timeout=30.0, check=check)
+
+        # Determine if the user's reaction corresponds to the correct answer
+        if str(reaction.emoji) == '1️⃣' and choices[0] == character_name:
+            await game_room.send(f'You got it right! The character is {character_name}.')
+            await game_room.send(character_image)
+            # Update user's attempts and correct answers in the database
+            await update_user_stats(ctx.user.id, 1, 1, ctx)
+        elif str(reaction.emoji) == '2️⃣' and choices[1] == character_name:
+            await game_room.send(f'You got it right! The character is {character_name}.')
+            await game_room.send(character_image)
+            # Update user's attempts and correct answers in the database
+            await update_user_stats(ctx.user.id, 1, 1, ctx)
+        elif str(reaction.emoji) == '3️⃣' and choices[2] == character_name:
+            await game_room.send(f'You got it right! The character is {character_name}.')
+            await game_room.send(character_image)
+            # Update user's attempts and correct answers in the database
+            await update_user_stats(ctx.user.id, 1, 1, ctx)
+        else:
+            await game_room.send(f'You got it wrong! The correct answer was {character_name}.')
+            await game_room.send(character_image)
+            # Update user's attempts in the database
+            await update_user_stats(ctx.user.id, 1, 0, ctx)
+    except asyncio.TimeoutError:
+        await game_room.send(f'You took too long to answer. The character was {character_name}.')
+        await game_room.send(character_image)
+        # Update user's attempts in the database
+        await update_user_stats(ctx.user.id, 1, 0, ctx)
+
+# Run the bot
 bot.run(TOKEN)
